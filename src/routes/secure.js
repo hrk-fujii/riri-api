862b800a6908d039ec83b66a2139a282c7d00f96
Hiroki Fujii <hfujii@hisystron.com>
2021-11-18 17:54:09 +0900

    使用しないルーティングをコメントアウト

--- src/routes/secure.js
+++ src/routes/secure.js
@@ -1,21 +1,21 @@
 var express = require('express');
-var middleware = require('./middlewares/api');
+// var middleware = require('./middlewares/api');
 var sendError = require('../utils/sendError');
 
-var createController = require('../controllers/secure/create');
-var deleteController = require('../controllers/secure/delete');
-var editController = require('../controllers/secure/edit');
+//var createController = require('../controllers/secure/create');
+//var deleteController = require('../controllers/secure/delete');
+//var editController = require('../controllers/secure/edit');
 
 var router = express.Router();
 
 
 /* POST MIDDLE */
-// router.use(middleware.auth);
+ // router.use(middleware.auth);
 
 /* POST API */
-router.post('/create', createController);
-router.post('/delete', deleteController);
-router.post('/edit', editController);
+//router.post('/create', createController);
+//router.post('/delete', deleteController);
+//router.post('/edit', editController);
 
 router.use((err, req, res, next) => {
   sendError(res, err);
